5d1e23e4dde5f5fc206dda08192465b0ce0a8a22
Sriram0591 <97426187+Sriram0591@users.noreply.github.com>
2022-05-24 22:58:22 +0530

    #include<stdio.h> int main() {     int n,x,i,c=0;     scanf("%d",&n);     while(n!=0)     {         scanf("%d",&x);         for(i=1;i<x;i++)         {             if((i*i)==x)             {                 c=1;                 printf("True ");                 break;             }             else             {                 c=0;             }         }         if(c==0)         {             printf("False ");         }         n--;     } }

---
+++ Valid_Perfect_Square.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,x,i,c=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        scanf("%d",&x);
+        for(i=1;i<x;i++)
+        {
+            if((i*i)==x)
+            {
+                c=1;
+                printf("True
+");
+                break;
+            }
+            else
+            {
+                c=0;
+            }
+        }
+        if(c==0)
+        {
+            printf("False
+");
+        }
+        n--;
+    }
+}
