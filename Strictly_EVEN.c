f6b22bc60463f99848e18bd316e192f80d1c4c26
Sriram0591 <97426187+Sriram0591@users.noreply.github.com>
2022-05-25 17:59:13 +0530

    #include<stdio.h> int main() {     int arr[100],n,i,s=0;     scanf("%d", &n);     for(i=0; i<n; i++)     {     	scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {         	if(i%2==1)         	{         		s=1;         		break; 			} 		}     }     if(s==0)     {     	printf("True"); 	} 	else 	{ 		printf("False"); 	} }

---
+++ Strictly_EVEN.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,i,s=0;
+    scanf("%d", &n);
+    for(i=0; i<n; i++)
+    {
+    	scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+        	if(i%2==1)
+        	{
+        		s=1;
+        		break;
+			}
+		}
+    }
+    if(s==0)
+    {
+    	printf("True");
+	}
+	else
+	{
+		printf("False");
+	}
+}
