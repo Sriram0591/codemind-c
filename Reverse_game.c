aa4732175459862140c4658f3080a12d1fe9f1ee
Sriram0591 <97426187+Sriram0591@users.noreply.github.com>
2022-05-25 21:54:12 +0530

    #include<stdio.h> int main() {     int arr[100],n,i,temp,rem,sum=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);         temp=arr[i];         while(temp!=0)         {             rem=temp%10;             sum=sum*10+rem;             temp=temp/10;         }         printf("%d ",sum);         sum=0;     }
    }

---
+++ Reverse_game.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,i,temp,rem,sum=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+        temp=arr[i];
+        while(temp!=0)
+        {
+            rem=temp%10;
+            sum=sum*10+rem;
+            temp=temp/10;
+        }
+        printf("%d ",sum);
+        sum=0;
+    }
+    
+}
