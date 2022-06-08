89cbcaff8e24ba29ea507e8b098bb39f8a24777c
Sriram0591 <97426187+Sriram0591@users.noreply.github.com>
2022-06-08 11:24:31 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(j==i||j==1||i==n)             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int n,i, j;
+    int n,i,j;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
         for(j=1;j<=n;j++)
         {
-            printf("%c ",'A'-1 + i);
+            if(j==i||j==1||i==n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
         }
         printf("
 ");
